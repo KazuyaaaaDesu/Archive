943c0cef7271b7b4cad9951abcda7b3490d4a47b
Aaron Jacob Gutierrez <gutierrezaaron291@gmail.com>
2026-07-14 12:29:30 +0800

    refactor: remove unecessary tailwind classes to improve readability

--- components/globals/Aside/Aside.tsx
+++ components/globals/Aside/Aside.tsx
@@ -1,11 +1,17 @@
 'use client'
 
-import { CollapsedFooter, ExpandedFooter } from '@/components/globals/Aside/AsideFooter'
-import LogoAside from '@/components/globals/Aside/AsideLogo'
+import {
+  CollapsedFooter,
+  ExpandedFooter,
+} from '@/components/globals/Aside/AsideFooter'
+import AsideLogo from '@/components/globals/Aside/AsideLogo'
 import { useAside } from '@/store/useAside'
 import { useSession } from 'next-auth/react'
 import { usePathname } from 'next/navigation'
-import { CollapsedNavLink, ExpandedNavLink } from '@/components/globals/Aside/AsideNavLink'
+import {
+  CollapsedNavLink,
+  ExpandedNavLink,
+} from '@/components/globals/Aside/AsideNavLink'
 
 export default function Aside() {
   const minimize = useAside((state) => state.minimize)
@@ -25,14 +31,18 @@ export default function Aside() {
     // Sidebar container
     <aside
       className={`hidden md:flex flex-col ${
-        minimize ? 'w-[72px]' : 'w-[264px]'
-      } bg-[#fafbff] border-r border-[#eceef8] shadow-[4px_0px_24px_0px_rgba(112,125,255,0.06),1px_0px_0px_0px_#eceef8] transition-all duration-300 shrink-0`}
+        minimize ? 'w-fit' : 'w-auto'
+      } bg-[#fafbff] border-r border-[#eceef8] shadow-[4px_0px_24px_0px_rgba(112,125,255,0.06),1px_0px_0px_0px_#eceef8] transition-all duration-300`}
     >
       {/* Logo + collapse button */}
-      <LogoAside />
+      <AsideLogo />
       {/* Navigation links */}
-      <nav className="flex-1 overflow-y-auto px-[8px] py-[10px]">
-        {minimize ? <CollapsedNavLink pathname={pathname} /> : <ExpandedNavLink pathname={pathname} />}
+      <nav className="flex-1 overflow-y-auto p-2.5">
+        {minimize ? (
+          <CollapsedNavLink pathname={pathname} />
+        ) : (
+          <ExpandedNavLink pathname={pathname} />
+        )}
       </nav>
 
       {minimize ? (
